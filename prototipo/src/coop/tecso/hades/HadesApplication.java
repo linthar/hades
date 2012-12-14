d4861d255e74031b6e6d949e54d6b642e16b96ac
linthar <fernando.d.garcia@gmail.com>
2012-12-14 12:32:19 -0300

    initial version

---
+++ prototipo/src/coop/tecso/hades/HadesApplication.java
@@ -0,0 +1,30 @@
+package coop.tecso.hades;
+
+import com.vaadin.Application;
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.CustomLayout;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class HadesApplication extends Application {
+	
+	@Override
+	public void init() {
+		
+		CustomLayout custom = new CustomLayout("panel-widgets");
+		 
+		
+		custom.addComponent(new Embedded("", new ThemeResource("images/warning-icon.png")), "warning-icon");
+		
+		
+		// Main window
+		Window mainWindow = new Window("Hades @ Tecso");
+		mainWindow.setSizeFull();
+		mainWindow.addComponent(custom);
+		setMainWindow(mainWindow);
+		
+		setTheme("hades-theme");
+	}
+
+}
